3ccfdeda20e6d2715936692abd17d34ae217288c
Kelvin-Ju <81310618+Kelvin-Ju@users.noreply.github.com>
2021-08-03 18:00:34 +0800

    Add files via upload

---
+++ options/run_model_opts_r2.py
@@ -0,0 +1,25 @@
+from .base_opts import BaseOpts
+class RunModelOpts(BaseOpts):
+    def __init__(self):
+        super(RunModelOpts, self).__init__()
+        self.initialize()
+
+    def initialize(self):
+        BaseOpts.initialize(self)
+        #### Model and Dataset ####
+        self.parser.add_argument('--run_model',   default=True, action='store_false')
+        # self.parser.add_argument('--benchmark',   default='Gargon')
+        self.parser.add_argument('--benchmark',   default='DiLiGenT_main_r2_2')
+        self.parser.add_argument('--bm_dir',      default='.data/datasets/DiLiGenT/pmsData')
+        self.parser.add_argument('--model',       default='GR_PSN_run')
+        self.parser.add_argument('--test_batch',  default=1,    type=int)
+        self.parser.add_argument('--mtrl_type',  default=1,    type=int)
+        #### Display Arguments ####
+        self.parser.add_argument('--test_intv',   default=1,    type=int) 
+        self.parser.add_argument('--test_disp',   default=1,    type=int) 
+        self.parser.add_argument('--test_save',   default=1,    type=int) 
+
+    def parse(self):
+        BaseOpts.parse(self)
+        self.args.in_img_num=96
+        return self.args
